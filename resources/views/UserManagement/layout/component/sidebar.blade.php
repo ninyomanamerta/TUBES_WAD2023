22329af60a611176678b5468d4271a7abdc0dff2
falkrii <muhnaufalfikri@gmail.com>
2023-12-28 09:57:55 +0700

    stepdua

---
+++ resources/views/UserManagement/layout/component/sidebar.blade.php
@@ -0,0 +1,73 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item" style="padding-top: 10px">
+            <a class="nav-link " href="{{ route('user-dashboard') }}">
+                <i class="bi bi-house-door"></i>
+                <span>Home</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{ route('user-cari') }}">
+                <i class="bi bi-search"></i>
+                <span>Cari</span>
+            </a>
+        </li><!-- End Register Page Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{ route('user-riwayat') }}">
+                <i class="bi bi-journals"></i>
+                <span>Riwayat</span>
+            </a>
+        </li><!-- End Register Page Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="{{ route('user-sumbang') }}">
+                <i class="bi bi-box2-heart"></i>
+                <span>Sumbang Buku</span>
+            </a>
+        </li><!-- End Register Page Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#">
+                <i class="bi bi-graph-up-arrow"></i>
+                <span>Literasi</span>
+            </a>
+        </li><!-- End Register Page Nav -->
+
+        <!-- <li class="nav-item">
+        <a class="nav-link collapsed" href="users-profile.php">
+          <i class="bi bi-person"></i>
+          <span>Profile</span>
+        </a>
+      </li>End Profile Page Nav -->
+
+        <!-- <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-login.php">
+          <i class="bi bi-box-arrow-in-right"></i>
+          <span>Login</span>
+        </a>
+      </li>End Login Page Nav
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-register.php">
+          <i class="bi bi-card-list"></i>
+          <span>Register</span>
+        </a>
+      </li>End Register Page Nav -->
+
+        <li class="nav-item" style="padding-top: 9cm">
+            <a class="nav-link collapsed" href="#" style="font-size: 12px; color: #808080;">
+                <span>Tentang Kami</span>
+            </a>
+        </li><!-- Tentang Kami Page Nav -->
+
+
+
+
+    </ul>
+
+</aside><!-- End Sidebar-->
---
+++ resources/views/UserManagement/layouts/master.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Laravel App</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/">Your App Name</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('books.index') }}">Book List</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('categories.index') }}">Categories</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('users.index') }}">Member List</a>
+                </li>
+                <!-- Add other navigation links as needed -->
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mt-4">
+        @yield('content')
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
